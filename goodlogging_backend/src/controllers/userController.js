4a96f1c40eca0d16880815127ab1f0967b514317
강현욱 <rkdgusdnr32@naver.com>
2022-07-27 16:38:33 +0900

    test

---
+++ goodlogging_backend/src/controllers/userController.js
@@ -0,0 +1,70 @@
+import httpStatus from "http-status";
+import {User} from "../../models";
+import jwt from "jsonwebtoken";
+import { passwordHash, passwordCompare } from "../middlewares/password";
+import { APIError } from "../errors/apierror";
+import errorCodes from "../errors/error";
+import asyncWrapper from "../errors/wrapper";
+
+const register = async (req, res) => {
+    const {username, email, password, level, address} = req.body;
+    const exist = await User.findOne({
+        where: {
+            email
+        }
+    });
+    //눙마ㅣㄴ어ㅏㅣㅁ너안미ㅓㅏㅣ너아ㅣㅁ너아ㅣㅁ너나어ㅏㅣ
+    if(exist){
+        throw new APIError(httpStatus.BAD_REQUEST, errorCodes.EMAIL_ALREADY_EXISTS);
+    }
+    const user = await User.create({
+        username,
+        email,
+        password: await passwordHash(password),
+        level,
+        address
+    });
+    return res.json({
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        level: user.level,
+        address: user.address
+    });
+}
+
+const login = async (req, res) => {
+    const {email, password} = req.body;
+    const exist = await User.findOne({
+        where: {
+            email
+        }
+    });
+    if(!exist){
+        throw new APIError(httpStatus.BAD_REQUEST, errorCodes.EMAIL_NOT_EXISTS)
+    }
+    const passwordCorrect = passwordCompare(password, exist.password);
+    if(!passwordCorrect){
+        throw new APIError(httpStatus.BAD_REQUEST, errorCodes.PASSWORD_NOT_MATCH)
+    }
+    const token = jwt.sign(
+        {
+            id: exist.id
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "5m",
+            issuer: "nodebird"
+        }
+    );
+    return res.json({
+        code: 200,
+        message: "토큰 발급 완료",
+        token
+    });
+}
+
+export default {
+    register: asyncWrapper(register),
+    login: asyncWrapper(login)
+}
